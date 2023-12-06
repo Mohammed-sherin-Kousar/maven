851485e531d2d266bb62ac1df94e5aefda64772a
sherin <sherinkousar17@gmail.com>
2023-12-06 16:09:32 +0530

    first commit

---
+++ src/main/java/org/fullstackacademy/App.java
@@ -0,0 +1,13 @@
+package org.fullstackacademy;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
---
+++ src/main/java/org/fullstackacademy/App1.java
@@ -0,0 +1,13 @@
+package org.fullstackacademy;
+
+/**
+ * Hello world!
+ *
+ */
+public class App1 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Welcome!" );
+    }
+}
